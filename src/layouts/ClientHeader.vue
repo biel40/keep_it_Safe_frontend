c2403e54a78224bb8268123f2fa7269d1c02e8ae
Javeir Viñas Garcia <javiervinasgarcia@gmail.com>
2019-04-20 15:11:24 +0200

    change structure of login and register cards

--- src/layouts/ClientHeader.vue
+++ src/layouts/ClientHeader.vue
@@ -20,61 +20,8 @@
       </q-tabs>
     </q-header>
 
-    <q-dialog v-model="login">
-      <q-card class="bg-grey-12" style="width: 800px; max-width:  90vw;">
-        <q-card-section class="col-12 bg-grey-4">
-          <div class="text-h5 text-center text-grey-9">¡Bien venido a Keep it Safe!</div>
-          <div class="text-subtitle2 text-center text-grey-9">Regístrate y podrás reservar equipaje gratuitamente</div>
-        </q-card-section>
-        <q-separator class="col-12"/>
-        <q-card-section class="col-12 row flex justify-center">
-
-          <div class="flex column no-wrap col-12 col-md text-h6" >
-            <div>
-              Correo electronico
-              <q-input outlined v-model="text" placeholder="demo@keepitsafe.com" class="q-py-md  "/>
-            </div> 
-            <div>
-              Contraseña
-              <q-input outlined type="password" v-model="text" class="q-py-md" placeholder="*********"/>
-            </div>
-            <q-btn color="primary" label="Registrarse" size="md"  class="button-login  q-mt-md"/>
-
-
-            <q-item clickable class="flex items-center justify-center q-mt-lg item-border-radius-40 q-pd-xs col" dense="">
-              <q-icon color="grey-9" name="mdi-account-circle-outline" size="50px" class="q-mr-md"/>
-              <div class="flex column no-wrap justify-center">
-                <div class="text-center text-weight-light text-body2">
-                  ¿No tienes cuenta?
-                </div>
-                <div class="text-center ">
-                  Registrate aquí
-                </div>
-              </div>
-            </q-item>
-          </div>
-
-          <q-separator vertical v-if="!$q.screen.lt.md" inset  class="q-mx-md"/>
-          <!-- TODO insual error, not apear horizontal q-spacer, but when adding padding style if it apear -->
-          <q-separator inset style="padding:0.25px;" class="q-my-md" v-else />
-
-          <div class="flex column no-wrap justify-center col-12 col-md text-h6 ">
-              <div class="q-mb-xs text-center text-body1 text-grey-9 text-weight-light">o si lo prefieres</div>
-              <q-btn class="q-my-md" color="blue-10" icon="mdi-facebook" size="lg" no-wrap align="left">
-                <div class="q-pl-xl">
-                  ENTRA CON FACEBOOK
-                </div>
-              </q-btn>
-              <q-btn class="q-mt-md" color="blue-7" icon="mdi-google" size="lg" no-wrap align="left">
-                <div class="q-pl-xl">
-                  ENTRA CON GOOGLE
-                </div>
-              </q-btn>
-          </div>
-          
-        </q-card-section>
-        
-      </q-card>
+    <q-dialog v-model="login" transition-show="slide-down">
+      <LoginCard/>
     </q-dialog>
 
     <q-page-container>
@@ -84,17 +31,15 @@
 </template>
 
 <script>
+import LoginCard from '../components/LoginCard'
 export default {
   data () {
     return {
       login: false,
-      text: '',
     }
-  },
-  mounted(){
-        console.log(this.$q.screen); 
-      },
-  methods:{
+  }, 
+  components:{
+    LoginCard
   }
 }
 </script>
