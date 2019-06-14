25f73ac379cf3633bb2f16d7102c7f008ff392d5
Biel Borras Serra <bielet40@gmail.com>
2019-06-14 21:08:13 +0200

    comments avanzado mucho

--- src/boot/classes.js
+++ src/boot/classes.js
@@ -36,6 +36,12 @@ export default ({
       this.user = user;
       this.luggages;
     },
+    Comments: function(comment_id,comment_text, user){
+        this.comment_id = comment_id;
+        this.comment_text = comment_text;
+        this.user = user;
+        this.isThisUserComment = false
+    },
     Utils: {
       verifyTokenSignature: function (token, user) {
         axios
--- src/components/ClientComments.vue
+++ src/components/ClientComments.vue
@@ -2,13 +2,13 @@
     <q-card style="height: 70vh; width: 400px; max-width: 90vw;" ref="card" class="background-color-app-light flex column no-wrap justify-between q-card-container q-mb-md"> 
       <q-item>
         <q-scroll-area ref="scrollAreaComments" style="height: 50vh; width: 350px; max-width: 70vw;" :thumb-style="scrollStyle">
-          <!-- TODO: Cambiar esto por la información de la tabla comments de MySQL.  -->
+          <!-- TODO: Hacer CSS para que se vean bien los mensajes. -->
           
-          <div v-for="number in 10" :key="number" class="q-py-xs q-pa-md">
-            Lorem ipsum dolor sit amet, consectetur adipisicing
-            elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua.
+          <div v-for="comment in comments" :key="comment.comment_id" class="q-py-xs q-pa-md">
+              <p v-if="comment.user">{{comment.user.name}} dijo : {{ comment.comment_text }} </p>
+              <p v-else> Anónimo dijo : {{ comment.comment_text }} </p>
           </div>
+
         </q-scroll-area>
       </q-item>
       
@@ -24,12 +24,38 @@
 </template>
 
 <script>
+
 export default {
   name: 'ClientComments',
   data () {
     return {
         model: '',
+        comments: []
     }
+  },
+  methods: {
+      getComments() {
+        this.$axios.get('http://localhost:8081/comments')
+        .then(response => {
+
+            // Hay que tener en cuenta que los comentarios, los atributos hay problemas con las instancias.
+            let commentTest = this.$classes.Comments = response.data;
+            
+            console.log("Comment Text ->" , commentTest);
+
+            this.comments = commentTest;
+
+            // Parsearlo, y hacer v-if para pintar los comentarios de forma diferente.
+
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+      }
+  },
+  created() {
+     this.getComments();
   },
   computed:{
     scrollStyle () {
@@ -42,8 +68,10 @@ export default {
     }
   },
   mounted(){
-    // TODO get height and change the 1000 number
-    this.$refs.scrollAreaComments.setScrollPosition (1000, 0) ;
+    // TODO: Get height and change the 1000 number
+    this.$refs.scrollAreaComments.setScrollPosition (1000, 0);
+
+    // document.querySelector(".scroll")
   }
 }
 </script>
